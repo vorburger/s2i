f4c40ed8ce2498c0535c27e478e68b934f42428c
Michael Vorburger <vorburger@redhat.com>
2019-02-08 17:55:28 +0100

    {WIP} java/examples/spring-gradle
    TODO produce WAR instead of JAR
    https://docs.spring.io/spring-boot/docs/current/gradle-plugin/reference/html/#packaging-executable-wars
    and get test.sh to actually pass (it currently fails)

---
+++ java/examples/spring-gradle/src/test/java/io/fabric8/s2i/example/spring/ApplicationTests.java
@@ -0,0 +1,17 @@
+package io.fabric8.s2i.example.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
